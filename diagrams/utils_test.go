a2c94a117040b0f429e532ec7e0ea2ccd1fd5291
DBX12 <dbx12@dbx12.de>
2021-08-13 09:47:05 +0200

    Add utils_test

---
+++ diagrams/utils_test.go
@@ -0,0 +1,63 @@
+package diagrams
+
+import (
+	"testing"
+)
+
+func TestMakeValidId(t *testing.T) {
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"replace invalid chars",
+			args{"a very-bad:identifier/really-bad"},
+			"avery_bad_identifier_really_bad",
+		},
+		{
+			"change nothing in valid id",
+			args{"a_valid_identifier"},
+			"a_valid_identifier",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MakeValidId(tt.args.id); got != tt.want {
+				t.Errorf("MakeValidId() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_assertValidId(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"Prevent whitespaces",
+			args{"model foo bar"},
+			true,
+		},
+		{
+			"Allow good names",
+			args{"model_foo"},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := assertValidId(tt.args.name); (err != nil) != tt.wantErr {
+				t.Errorf("assertValidId() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
